c88fba7de23f168f8a7577b58c1e718719e10a90
Larissa Loureiro <larissa.loureiro@ymail.com>
2024-02-11 20:32:03 -0300

    Isolando a inicialização da app para facilitar a execução dos testes, e alterando mensagem de erro no verifyJWT

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-const express = require('express')
-const routes = require('./routes')
+const app = require('./app.js')
+require('dotenv').config();
 
-const app = express()
-const port = 3000
-
-routes(app)
+const port = process.env.APP_PORT
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 })
-
-module.exports = app
